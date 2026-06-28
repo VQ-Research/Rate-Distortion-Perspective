977dedbb286c6cd9ff268bf48700f4e3c2651cf1
sunset-clouds <xianghong_fang@163.com>
2026-06-27 23:59:46 -0400

    6.27 updates

---
+++ VAR-Transplant/scripts/transplant/FFHQ/lfq_ffhq_transplant.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=lfq_transplant
+#SBATCH --account=aip-rudner
+#SBATCH --partition=gpubase_h100_b2,gpubase_h100_b3,gpubase_h100_b4,gpubase_h100_b5
+#SBATCH --nodes=1
+#SBATCH --mem=50gb
+#SBATCH --cpus-per-task 10
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:h100:2
+#SBATCH --output /project/6105494/sunset/VQ-Projects/VQ-Transplant/slurm/Transplant/ImageNet/lfq_transplant.out
+#SBATCH --error /project/6105494/sunset/VQ-Projects/VQ-Transplant/slurm/Transplant/ImageNet/lfq_transplant.err
+
+#!/bin/bash
+#SBATCH --job-name=bsq_ffhq_transplant
+#SBATCH --account=aip-rudner
+#SBATCH --partition=gpubase_h100_b2,gpubase_h100_b3,gpubase_h100_b4,gpubase_h100_b5
+#SBATCH --nodes=1
+#SBATCH --mem=80gb
+#SBATCH --cpus-per-task 10
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:h100:2
+#SBATCH --output /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/VAR-Transplant/slurm/Transplant/FFHQ/bsq_ffhq_transplant.out
+#SBATCH --error /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/VAR-Transplant/slurm/Transplant/FFHQ/bsq_ffhq_transplant.err
+
+module load gcc opencv/4.8.1
+source /home/sunset/environment/VQ-Tokenizer/bin/activate
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=13583 train_SQ_transplant.py --VQ=lfq --dataset_name=ImageNet --global_batch_size=64  --L=2 --project_dim=16 --stage=transplant --beta=1.0
---
+++ VAR-Transplant/scripts/transplant/FFHQ/wasserstein_ffhq_pq.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=wasserstein_pq
+#SBATCH --account=aip-rudner
+#SBATCH --partition=gpubase_h100_b2,gpubase_h100_b3,gpubase_h100_b4,gpubase_h100_b5
+#SBATCH --nodes=1
+#SBATCH --mem=50gb
+#SBATCH --cpus-per-task 10
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:h100:2
+#SBATCH --output /project/6105494/sunset/VQ-Projects/VQ-Transplant/slurm/Transplant/ImageNet/wasserstein_pq.out
+#SBATCH --error /project/6105494/sunset/VQ-Projects/VQ-Transplant/slurm/Transplant/ImageNet/wasserstein_pq.err
+
+#!/bin/bash
+#SBATCH --job-name=bsq_ffhq_transplant
+#SBATCH --account=aip-rudner
+#SBATCH --partition=gpubase_h100_b2,gpubase_h100_b3,gpubase_h100_b4,gpubase_h100_b5
+#SBATCH --nodes=1
+#SBATCH --mem=80gb
+#SBATCH --cpus-per-task 10
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:h100:2
+#SBATCH --output /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/VAR-Transplant/slurm/Transplant/FFHQ/bsq_ffhq_transplant.out
+#SBATCH --error /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/VAR-Transplant/slurm/Transplant/FFHQ/bsq_ffhq_transplant.err
+
+module load gcc opencv/4.8.1
+source /home/sunset/environment/VQ-Tokenizer/bin/activate
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=13588 train_PQ_transplant.py --VQ=wasserstein_vq --dataset_name=ImageNet --global_batch_size=64 --codebook_size 256  --codebook_dim=8 --pq=2 --stage=transplant --alpha=1.0 --beta=0.2 --gamma=0.5
