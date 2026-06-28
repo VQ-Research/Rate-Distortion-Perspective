ec4eb1f373b3234082315543c3f9d7c438b140a6
sunset-clouds <xianghong_fang@163.com>
2026-06-28 19:59:36 -0400

    2026-6-28 updates

---
+++ VAR-Transplant/scripts/refinement/FFHQ/fsq_ffhq_refinement.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=fsq_ffhq_transplant
+#SBATCH --account=aip-rudner
+#SBATCH --partition=gpubase_h100_b2,gpubase_h100_b3,gpubase_h100_b4,gpubase_h100_b5
+#SBATCH --nodes=1
+#SBATCH --mem=80gb
+#SBATCH --cpus-per-task 10
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:h100:2
+#SBATCH --output /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/VAR-Transplant/slurm/Transplant/FFHQ/fsq_ffhq_transplant.out
+#SBATCH --error /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/VAR-Transplant/slurm/Transplant/FFHQ/fsq_ffhq_transplant.err
+
+module load gcc opencv/4.8.1
+source /home/sunset/environment/VQ-Tokenizer/bin/activate
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=13582 train_SQ_transplant.py --VQ=fsq --dataset_name=FFHQ --global_batch_size=64  --L=4 --project_dim=8 --stage=transplant --beta=1.0
---
+++ VAR-Transplant/scripts/refinement/FFHQ/mmd_vq_ffhq_refinement.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=mmd_vq_ffhq_transplant
+#SBATCH --account=aip-rudner
+#SBATCH --partition=gpubase_h100_b2,gpubase_h100_b3,gpubase_h100_b4,gpubase_h100_b5
+#SBATCH --nodes=1
+#SBATCH --mem=80gb
+#SBATCH --cpus-per-task 10
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:h100:2
+#SBATCH --output /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/VAR-Transplant/slurm/Transplant/FFHQ/mmd_vq_ffhq_transplant.out
+#SBATCH --error /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/VAR-Transplant/slurm/Transplant/FFHQ/mmd_vq_ffhq_transplant.err
+
+module load gcc opencv/4.8.1
+source /home/sunset/environment/VQ-Tokenizer/bin/activate
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --master_port=13592 train_VQ_transplant.py --VQ=mmd_vq --dataset_name=FFHQ --global_batch_size=64 --codebook_size=65536 --codebook_dim=16 --pq=1 --stage=transplant --alpha=1.0 --beta=0.2 --gamma=0.5
