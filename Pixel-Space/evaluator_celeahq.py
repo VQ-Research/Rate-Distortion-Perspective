0751d6f1e2d48604c91e2c3aab823600633bcb63
sunset-clouds <xianghong_fang@163.com>
2026-07-17 23:33:09 -0400

    2026-07-17

--- Pixel-Space/evaluator_celeahq.py
+++ Pixel-Space/evaluator_celeahq.py
@@ -11,29 +11,384 @@ from PIL import Image, ImageOps, ImageFilter
 import config
 from cleanfid import fid
 
-input_dir = "/projects/yuanai/projects/VQ-Transplant3/reconstruction/CelebAHQ"
+input_dir = "/project/6105494/shared/reconstruction/CelebAHQ"
 
-mmd_transplant_16384 = "/projects/yuanai/projects/VQ-Transplant3/reconstruction/Transplant/CelebAHQ/mmd_vq_transplant_16384_False"
-wasserstein_transplant_16384 = "/projects/yuanai/projects/VQ-Transplant3/reconstruction/Transplant/CelebAHQ/wasserstein_vq_transplant_16384_False"
-
-mmd_refinement_16384 = "/projects/yuanai/projects/VQ-Transplant3/reconstruction/Refinement/CelebAHQ/mmd_vq_refinement_16384_False"
-wasserstein_refinement_16384 = "/projects/yuanai/projects/VQ-Transplant3/reconstruction/Refinement/CelebAHQ/wasserstein_vq_refinement_16384_False"
-
-
-print("#################transplant-stage###########################")
-print(mmd_transplant_16384)
-FID = fid.compute_fid(mmd_transplant_16384, input_dir)
+print("#################VQ method###########################")
+## Vanilla VQ
+Vanilla_VQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_65536_5"
+Vanilla_VQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_65536_10"
+Vanilla_VQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_65536_15"
+Vanilla_VQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_65536_20"
+Vanilla_VQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_65536_25"
+Vanilla_VQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_65536_30"
+print(Vanilla_VQ_5)
+FID = fid.compute_fid(Vanilla_VQ_5, input_dir)
 print("FID: "+str(FID))
 
-print(wasserstein_transplant_16384)
-FID = fid.compute_fid(wasserstein_transplant_16384, input_dir)
+print(Vanilla_VQ_10)
+FID = fid.compute_fid(Vanilla_VQ_10, input_dir)
 print("FID: "+str(FID))
 
-print("#################Refinement-stage###########################")
-print(mmd_refinement_16384)
-FID = fid.compute_fid(mmd_refinement_16384, input_dir)
+print(Vanilla_VQ_15)
+FID = fid.compute_fid(Vanilla_VQ_15, input_dir)
 print("FID: "+str(FID))
 
-print(wasserstein_refinement_16384)
-FID = fid.compute_fid(wasserstein_refinement_16384, input_dir)
+print(Vanilla_VQ_20)
+FID = fid.compute_fid(Vanilla_VQ_20, input_dir)
 print("FID: "+str(FID))
+
+print(Vanilla_VQ_25)
+FID = fid.compute_fid(Vanilla_VQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(Vanilla_VQ_30)
+FID = fid.compute_fid(Vanilla_VQ_30, input_dir)
+print("FID: "+str(FID))
+
+##EMA VQ
+EMA_VQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_65536_5"
+EMA_VQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_65536_10"
+EMA_VQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_65536_15"
+EMA_VQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_65536_20"
+EMA_VQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_65536_25"
+EMA_VQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_65536_30"
+print(EMA_VQ_5)
+FID = fid.compute_fid(EMA_VQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_VQ_10)
+FID = fid.compute_fid(EMA_VQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_VQ_15)
+FID = fid.compute_fid(EMA_VQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_VQ_20)
+FID = fid.compute_fid(EMA_VQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_VQ_25)
+FID = fid.compute_fid(EMA_VQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_VQ_30)
+FID = fid.compute_fid(EMA_VQ_30, input_dir)
+print("FID: "+str(FID))
+
+##Wasserstein VQ
+Wasserstein_VQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_65536_5"
+Wasserstein_VQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_65536_10"
+Wasserstein_VQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_65536_15"
+Wasserstein_VQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_65536_20"
+Wasserstein_VQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_65536_25"
+Wasserstein_VQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_65536_30"
+print(Wasserstein_VQ_5)
+FID = fid.compute_fid(Wasserstein_VQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_VQ_10)
+FID = fid.compute_fid(Wasserstein_VQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_VQ_15)
+FID = fid.compute_fid(Wasserstein_VQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_VQ_20)
+FID = fid.compute_fid(Wasserstein_VQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_VQ_25)
+FID = fid.compute_fid(Wasserstein_VQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_VQ_30)
+FID = fid.compute_fid(Wasserstein_VQ_30, input_dir)
+print("FID: "+str(FID))
+
+##MMD VQ
+MMD_VQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_65536_5"
+MMD_VQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_65536_10"
+MMD_VQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_65536_15"
+MMD_VQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_65536_20"
+MMD_VQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_65536_25"
+MMD_VQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_65536_30"
+print(MMD_VQ_5)
+FID = fid.compute_fid(MMD_VQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_VQ_10)
+FID = fid.compute_fid(MMD_VQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_VQ_15)
+FID = fid.compute_fid(MMD_VQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_VQ_20)
+FID = fid.compute_fid(MMD_VQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_VQ_25)
+FID = fid.compute_fid(MMD_VQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_VQ_30)
+FID = fid.compute_fid(MMD_VQ_30, input_dir)
+print("FID: "+str(FID))
+
+
+
+print("#################PQ method###########################")
+##Vanilla PQ
+Vanilla_PQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_2_5"
+Vanilla_PQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_2_10"
+Vanilla_PQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_2_15"
+Vanilla_PQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_2_20"
+Vanilla_PQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_2_25"
+Vanilla_PQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/vanilla_vq_2_30"
+print(Vanilla_PQ_5)
+FID = fid.compute_fid(Vanilla_PQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(Vanilla_PQ_10)
+FID = fid.compute_fid(Vanilla_PQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(Vanilla_PQ_15)
+FID = fid.compute_fid(Vanilla_PQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(Vanilla_PQ_20)
+FID = fid.compute_fid(Vanilla_PQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(Vanilla_PQ_25)
+FID = fid.compute_fid(Vanilla_PQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(Vanilla_PQ_30)
+FID = fid.compute_fid(Vanilla_PQ_30, input_dir)
+print("FID: "+str(FID))
+
+
+##EMA PQ
+EMA_PQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_2_5"
+EMA_PQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_2_10"
+EMA_PQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_2_15"
+EMA_PQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_2_20"
+EMA_PQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_2_25"
+EMA_PQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/ema_vq_2_30"
+print(EMA_PQ_5)
+FID = fid.compute_fid(EMA_PQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_PQ_10)
+FID = fid.compute_fid(EMA_PQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_PQ_15)
+FID = fid.compute_fid(EMA_PQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_PQ_20)
+FID = fid.compute_fid(EMA_PQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_PQ_25)
+FID = fid.compute_fid(EMA_PQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(EMA_PQ_30)
+FID = fid.compute_fid(EMA_PQ_30, input_dir)
+print("FID: "+str(FID))
+
+##Online PQ
+Online_PQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/online_vq_2_5"
+Online_PQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/online_vq_2_10"
+Online_PQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/online_vq_2_15"
+Online_PQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/online_vq_2_20"
+Online_PQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/online_vq_2_25"
+Online_PQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/online_vq_2_30"
+
+print(Online_PQ_5)
+FID = fid.compute_fid(Online_PQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(Online_PQ_10)
+FID = fid.compute_fid(Online_PQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(Online_PQ_15)
+FID = fid.compute_fid(Online_PQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(Online_PQ_20)
+FID = fid.compute_fid(Online_PQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(Online_PQ_25)
+FID = fid.compute_fid(Online_PQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(Online_PQ_30)
+FID = fid.compute_fid(Online_PQ_30, input_dir)
+print("FID: "+str(FID))
+
+##Wasserstein PQ
+Wasserstein_PQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_2_5"
+Wasserstein_PQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_2_10"
+Wasserstein_PQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_2_15"
+Wasserstein_PQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_2_20"
+Wasserstein_PQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_2_25"
+Wasserstein_PQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/wasserstein_vq_2_30"
+print(Wasserstein_PQ_5)
+FID = fid.compute_fid(Wasserstein_PQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_PQ_10)
+FID = fid.compute_fid(Wasserstein_PQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_PQ_15)
+FID = fid.compute_fid(Wasserstein_PQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_PQ_20)
+FID = fid.compute_fid(Wasserstein_PQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_PQ_25)
+FID = fid.compute_fid(Wasserstein_PQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(Wasserstein_PQ_30)
+FID = fid.compute_fid(Wasserstein_PQ_30, input_dir)
+print("FID: "+str(FID))
+
+##MMD PQ
+MMD_PQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_2_5"
+MMD_PQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_2_10"
+MMD_PQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_2_15"
+MMD_PQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_2_20"
+MMD_PQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_2_25"
+MMD_PQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/mmd_vq_2_30"
+print(MMD_PQ_5)
+FID = fid.compute_fid(MMD_PQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_PQ_10)
+FID = fid.compute_fid(MMD_PQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_PQ_15)
+FID = fid.compute_fid(MMD_PQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_PQ_20)
+FID = fid.compute_fid(MMD_PQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_PQ_25)
+FID = fid.compute_fid(MMD_PQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(MMD_PQ_30)
+FID = fid.compute_fid(MMD_PQ_30, input_dir)
+print("FID: "+str(FID))
+
+print("#################SQ method###########################")
+##FSQ
+FSQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/fsq_8_4_5"
+FSQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/fsq_8_4_10"
+FSQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/fsq_8_4_15"
+FSQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/fsq_8_4_20"
+FSQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/fsq_8_4_25"
+FSQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/fsq_8_4_30"
+print(FSQ_5)
+FID = fid.compute_fid(FSQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(FSQ_10)
+FID = fid.compute_fid(FSQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(FSQ_15)
+FID = fid.compute_fid(FSQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(FSQ_20)
+FID = fid.compute_fid(FSQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(FSQ_25)
+FID = fid.compute_fid(FSQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(FSQ_30)
+FID = fid.compute_fid(FSQ_30, input_dir)
+print("FID: "+str(FID))
+
+##BSQ
+BSQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/bsq_16_2_5"
+BSQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/bsq_16_2_10"
+BSQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/bsq_16_2_15"
+BSQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/bsq_16_2_20"
+BSQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/bsq_16_2_25"
+BSQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/bsq_16_2_30"
+print(BSQ_5)
+FID = fid.compute_fid(BSQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(BSQ_10)
+FID = fid.compute_fid(BSQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(BSQ_15)
+FID = fid.compute_fid(BSQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(BSQ_20)
+FID = fid.compute_fid(BSQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(BSQ_25)
+FID = fid.compute_fid(BSQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(BSQ_30)
+FID = fid.compute_fid(BSQ_30, input_dir)
+print("FID: "+str(FID))
+
+##LFQ
+LFQ_5 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/lfq_16_2_5"
+LFQ_10 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/lfq_16_2_10"
+LFQ_15 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/lfq_16_2_15"
+LFQ_20 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/lfq_16_2_20"
+LFQ_25 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/lfq_16_2_25"
+LFQ_30 = "/project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/reconstruction/CelebAHQ/lfq_16_2_30"
+print(LFQ_5)
+FID = fid.compute_fid(LFQ_5, input_dir)
+print("FID: "+str(FID))
+
+print(LFQ_10)
+FID = fid.compute_fid(LFQ_10, input_dir)
+print("FID: "+str(FID))
+
+print(LFQ_15)
+FID = fid.compute_fid(LFQ_15, input_dir)
+print("FID: "+str(FID))
+
+print(LFQ_20)
+FID = fid.compute_fid(LFQ_20, input_dir)
+print("FID: "+str(FID))
+
+print(LFQ_25)
+FID = fid.compute_fid(LFQ_25, input_dir)
+print("FID: "+str(FID))
+
+print(LFQ_30)
+FID = fid.compute_fid(LFQ_30, input_dir)
+print("FID: "+str(FID))
+
---
+++ Pixel-Space/rFID.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=rFID_celebahq
+#SBATCH --account=aip-rudner
+#SBATCH --partition=gpubase_l40s_b1,gpubase_l40s_b2,gpubase_l40s_b3
+#SBATCH --nodes=1
+#SBATCH --mem=80gb
+#SBATCH --cpus-per-task 10
+#SBATCH --time=3:00:00
+#SBATCH --gres=gpu:l40s:1
+#SBATCH --output /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/metrics/CelebAHQ/rFID_celebahq.out
+#SBATCH --error /project/6105494/sunset/VQ-Projects/Rate-Distortion-Perspective/Pixel-Space/metrics/CelebAHQ/rFID_celebahq.err
+
+module load gcc opencv/4.8.1
+source /home/sunset/environment/VQ-Tokenizer/bin/activate
+CUDA_VISIBLE_DEVICES="0" python -m torch.distributed.launch --nproc_per_node=1 --master_port=15581 evaluator_celeahq.py
